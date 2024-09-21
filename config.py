6ded9d9555cc4e1b6a3e097cc15a94f660b60983
mohits-git <mohitkhatri2805@gmail.com>
2024-09-21 23:26:21 +0530

    feat: creating the plot embedding for the moview database and performing query based on the plot embeddings

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ load_dotenv()
 
 DATABASE_URI = os.environ["DATABASE_URL"]
 HF_ACCESS_TOKEN = os.environ["HF_ACCESS_TOKEN"]
+EMBEDDING_URL = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import pymongo
 import requests
 
-from config import DATABASE_URI, HF_ACCESS_TOKEN
+from config import DATABASE_URI, EMBEDDING_URL, HF_ACCESS_TOKEN
 
 client = pymongo.MongoClient(DATABASE_URI)
 db = client.sample_mflix
 collection = db.movies
 
 
-embedding_url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
-
-
 def generate_embedding(text: str) -> list[float]:
     response = requests.post(
-        embedding_url,
+        EMBEDDING_URL,
         headers={"Authorization": f"Bearer {HF_ACCESS_TOKEN}"},
         json={"inputs": text}
     )
@@ -27,4 +24,26 @@ def generate_embedding(text: str) -> list[float]:
     return response.json()
 
 
-print(generate_embedding("Hello, World!"))
+def create_plot_embeddings():
+    for doc in collection.find({'plot': {"$exists": True}}).limit(50):
+        doc['plot_embedding'] = generate_embedding(doc['plot'])
+        collection.replace_one({'_id': doc['_id']}, doc)
+
+
+def query_plot(query: str):
+    results = collection.aggregate([
+        {
+            "$vectorSearch": {
+                "queryVector": generate_embedding(query),
+                "path": "plot_embedding",
+                "numCandidates": 100,
+                "limit": 4,
+                "index": "PlotSemanticSearch"
+            },
+        },
+    ])
+    for doc in results:
+        print(f"Movie Name: {doc['title']}, \nMovie Plot: {doc['plot']}\n")
+
+
+query_plot("imaginary characters from outer space at war")
