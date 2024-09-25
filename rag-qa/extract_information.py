d8d6e0dcec4cc85bfc9e67eaa30c8d7878e9ddf2
mohits-git <mohitkhatri2805@gmail.com>
2024-09-25 21:15:10 +0530

    feat: generate vector embedding with OpenAI embeddings and store it to the MongoDB

--- rag-qa/extract_information.py
+++ rag-qa/extract_information.py
@@ -1,9 +1,22 @@
 from pymongo import MongoClient
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import MongoDBAtlasVectorSearch
-from langchain.document_loaders import DirectoryLoader
-from langchain.llms import OpenAI
+from langchain_community.embeddings.openai import OpenAIEmbeddings
+from langchain_community.vectorstores import MongoDBAtlasVectorSearch
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.llms import OpenAI
 from langchain.chains import RetrievalQA
 import gradio as gr
 from gradio.themes.base import Base
 import config
+
+client = MongoClient(config.DATABASE_URL)
+dbName = "langchain_demo"
+collectionName = "collection_of_text_blobs"
+collection = client[dbName][collectionName]
+
+loader = DirectoryLoader('./sample_files', glob="./*.txt", show_progress=True)
+data = loader.load()
+
+embeddings = OpenAIEmbeddings(openai_api_key=config.OPENAI_API_KEY)
+
+vectorStore = MongoDBAtlasVectorSearch.from_documents(
+    data, embeddings, collection=collection)
